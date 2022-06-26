84ae377288e2cdf6681fbf08bf79cc6808da1a29
Alexandre Wermann <alexandre.wermann@sap.com>
2022-06-26 14:26:05 -0300

    update mqtt devices

--- devices/mqtt-devices/modules/mqtt.py
+++ devices/mqtt-devices/modules/mqtt.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 import json
 from paho.mqtt import client as paho_mqtt_client
 
@@ -10,7 +11,7 @@ class MQTTClient:
         self.broker = broker
         self.port = port
         self.topic = topic
-        self.message = { "priority": 1, "message": ""}
+        self.message = { "timestamp": 1, "message": "", "priority": 1}
 
     def connect_mqtt(self):
         def on_connect(client, userdata, flags, rc) -> None:
@@ -23,20 +24,26 @@ class MQTTClient:
         self.mqttclient.connect(self.broker, self.port)
         return self.mqttclient
 
-    def build_message(self, msg_count, high_priority_frequency):
+    def generate_payload_size(self, nbytes):
+        payload = "-"
+        for i in range(nbytes):
+            payload += "a"
+        return payload
+
+    def build_message(self, msg_count):
         msg = self.message
-        msg["message"] = msg_count
-        msg["priority"] = (msg_count % 2) + 1
+        msg["message"] = str(msg_count) + self.generate_payload_size(500)
+        msg["timestamp"] = datetime.datetime.now().isoformat()
         return json.dumps(msg)
 
     # n_messages: total amount of messages
     # message_period: period of time between each message (seconds)
     # high_priority_frequency: [0,1], percentage of priority 1 against priority 2 messages
-    def publish(self, n_messages, message_period, high_priority_frequency):
+    def publish(self, n_messages, message_period):
         msg_count = 0
         while n_messages > msg_count:
             time.sleep(message_period)
-            msg = self.build_message(msg_count=msg_count, high_priority_frequency=high_priority_frequency)
+            msg = self.build_message(msg_count=msg_count)
             result = self.mqttclient.publish(self.topic, msg)
             status = result[0]
             if status == 0:
--- devices/mqtt-devices/publisher.py
+++ devices/mqtt-devices/publisher.py
@@ -1,15 +1,22 @@
 import random
+import sys
 from modules.mqtt import MQTTClient
 
-broker = '192.168.1.195'
+if len(sys.argv) < 4:
+    print("Missing paremeters: TOPIC_NAME, NUM_MESSAGES, MESSAGE_PERIOD")
+    exit()
+
+broker = '192.168.1.153'
 port = 1883
-topic = "mytopic"
+topic = sys.argv[1]
+n_messages = int(sys.argv[2])
+message_period = float(sys.argv[3])
 client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 def run():
     client = MQTTClient(client_id, broker, port, topic)
     client.connect_mqtt()
-    client.publish(n_messages=1, message_period=1, high_priority_frequency=0.1)
+    client.publish(n_messages=n_messages, message_period=message_period)
 
 if __name__ == '__main__':
     run()
