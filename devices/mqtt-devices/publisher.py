ff753df916a2bb160e810c39bfbb842f30aca42d
Alexandre Wermann <alexandre.wermann@sap.com>
2022-07-25 23:08:57 -0300

    fix: add multi process workload script

--- devices/mqtt-devices/publisher.py
+++ devices/mqtt-devices/publisher.py
@@ -6,14 +6,14 @@ if len(sys.argv) < 4:
     print("Missing paremeters: TOPIC_NAME, NUM_MESSAGES, MESSAGE_PERIOD")
     exit()
 
-broker = '192.168.1.153'
+broker = 'localhost'
 port = 1883
 topic = sys.argv[1]
 n_messages = int(sys.argv[2])
 message_period = float(sys.argv[3])
-client_id = f'python-mqtt-{random.randint(0, 1000)}'
 
 def run():
+    client_id = f'python-mqtt-{random.randint(0, 1000)}'
     client = MQTTClient(client_id, broker, port, topic)
     client.connect_mqtt()
     client.publish(n_messages=n_messages, message_period=message_period)
